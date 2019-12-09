ad19d902539bf239d34738f691201135834cdd6c
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2019-12-08 16:52:35 -0800

    finished MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -13,7 +15,7 @@ const MovieList = props => {
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
+    };
     
     getMovies();
   }, []);
@@ -21,11 +23,13 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+         <Link to={`movies/${movie.id}`} key={movie.id}>
+         <MovieDetails key={movie.id} movie={movie} />
+       </Link>
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
